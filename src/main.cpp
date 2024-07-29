865b8e9ffedec02192cf7d981e7981a88ab72fb8
damoreluc <damore.luca@isisbassafriulana.edu.it>
2024-07-29 09:52:32 +0200

    Due encoder

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include <esp32_pcnt.h>
 /*
- * Conteggio encoder incrementale x1 mediante modulo PCNT #0 - solo canale ch0
+ * Conteggio due encoder incrementali x1 mediante modulo PCNT #0 - solo canale ch0
  *  impiego dei pin:
- *    pin_sig_ch0   sul GPIO_NUM_14
- *    pin_ctrl_ch0  sul GPIO_NUM_12
+ *   Encoder 0:
+ *    pin_sig0_ch0   sul GPIO_NUM_16
+ *    pin_ctrl0_ch0  sul GPIO_NUM_17
+ * 
+ *   Encoder 1:
+ *    pin_sig1_ch0   sul GPIO_NUM_12
+ *    pin_ctrl1_ch0  sul GPIO_NUM_14
  *
  * visualizzazione del conteggio up/down su seriale.
  * 
@@ -73,38 +78,47 @@
  *      //only use channel 0 of each counter unit.
  */
 // linee di input dell'encoder:
-#define pin_sig_ch0 GPIO_NUM_14
-#define pin_ctrl_ch0 GPIO_NUM_12
-#define pin_led GPIO_NUM_13
+#define pin_sig0_ch0 GPIO_NUM_16
+#define pin_ctrl0_ch0 GPIO_NUM_17
+#define pin_led GPIO_NUM_18
+
+#define pin_sig1_ch0 GPIO_NUM_12
+#define pin_ctrl1_ch0 GPIO_NUM_14
 
 // counter threshold
 #define threshold_0 10
 #define threshold_1 20
 #define high_limit 30
 
-// create counter object
-PulseCounter pc0;
+// create counter objects
+PulseCounter pc0, pc1;
 
 // gestione interrupt del counter0
 volatile bool pc0_int_flag = false; // counter 0 interrupt flag
+// gestione interrupt del counter1
+volatile bool pc1_int_flag = false; // counter 1 interrupt flag
 
 // mutex for hardware pulse counters
 portMUX_TYPE pcntMux0 = portMUX_INITIALIZER_UNLOCKED;
-
-// exit value of Install PCNT ISR service
-esp_err_t err_exit_pcnt_isr;
+portMUX_TYPE pcntMux1 = portMUX_INITIALIZER_UNLOCKED;
 
 // pulse counter ISR
 IRAM_ATTR void pc0_isr(void *);
+IRAM_ATTR void pc1_isr(void *);
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Conteggio up/down encoder su pin GPIO_14 e pin GPIO_12");
+  Serial.println("Conteggio up/down encoders incrementali");
+  Serial.println("  Encoder #0 su pin GPIO_16 e pin GPIO_17");
+  Serial.println("  Encoder #1 su pin GPIO_14 e pin GPIO_12");
   pinMode(pin_led, OUTPUT);
-  pinMode(pin_sig_ch0, INPUT_PULLUP);
-  pinMode(pin_ctrl_ch0, INPUT_PULLUP);
+
+  pinMode(pin_sig0_ch0, INPUT_PULLUP);
+  pinMode(pin_ctrl0_ch0, INPUT_PULLUP);
+  pinMode(pin_sig1_ch0, INPUT_PULLUP);
+  pinMode(pin_ctrl1_ch0, INPUT_PULLUP);
 
   digitalWrite(pin_led, HIGH);
   delay(2000);
@@ -113,16 +127,20 @@ void setup()
   // setup hardware pulse counter
   // initialise counter unit 0, channel 0 with signal input GPIO pin and control signal input pin
   // Note: (0 = no control signal input)
-  pc0.initialise(pin_sig_ch0, pin_ctrl_ch0);
+  pc0.initialise(pin_sig0_ch0, pin_ctrl0_ch0);
+  // initialise counter unit 0, channel 0 with signal input GPIO pin and control signal input pin
+  pc1.initialise(pin_sig1_ch0, pin_ctrl1_ch0);
 
   // count up on negative edges when ctrl is H,
   // count down on negative edges when ctrl is L,
   // count down on positive edges when ctrl is H,
   // count up on positive edges when ctrl is L
   pc0.set_mode(PCNT_COUNT_DEC, PCNT_COUNT_INC, PCNT_MODE_KEEP, PCNT_MODE_REVERSE);
+  pc1.set_mode(PCNT_COUNT_DEC, PCNT_COUNT_INC, PCNT_MODE_KEEP, PCNT_MODE_REVERSE);
 
   // set glich filter to ignore pulses less than 1000 x 2.5ns
   pc0.set_filter_value(1000);
+  pc1.set_filter_value(1000);
 
   // set and enable threshold 0 and 1 watch points - these will trigger an interrupt.
   // the event can be disabled and enabled using pc0.event_disable(PCNT_EVT_THRES_0) or pc0.event_enable(PCNT_EVT_THRES_0)
@@ -131,25 +149,34 @@ void setup()
   pc0.set_event_value(PCNT_EVT_THRES_1, threshold_1);
   pc0.set_event_value(PCNT_EVT_H_LIM, high_limit);
 
+  pc1.set_event_value(PCNT_EVT_THRES_0, threshold_0);
+  pc1.set_event_value(PCNT_EVT_THRES_1, threshold_1);
+  pc1.set_event_value(PCNT_EVT_H_LIM, high_limit);  
+
   // register interrupt service routine for this counter unit
   pc0.attach_interrupt(pc0_isr);
+  pc1.attach_interrupt(pc1_isr);
 
   // show isr pointer
-  Serial.printf("Pointer to pc0_isr: %p\n", pc0_isr);
+  //Serial.printf("Pointer to pc0_isr: %p\n", pc0_isr);
   //while(1);
 
   pc0.interrupt_enable();
+  pc1.interrupt_enable();
 
   // clear and restart the counter
   pc0.clear();
   pc0.resume();
+  pc1.clear();
+  pc1.resume();
 
   Serial.println("Start");
 }
 
 void loop()
 {
-  Serial.printf("Count: %d\t event status: %d\n", pc0.get_value(), pc0.event_status());
+  Serial.printf("Enc #0, Count: %d\t event status: %d\t", pc0.get_value(), pc0.event_status());
+  Serial.printf("Enc #1, Count: %d\t event status: %d\n", pc1.get_value(), pc1.event_status());
   if (pc0_int_flag)
   {
     pc0_int_flag = false;
@@ -169,10 +196,30 @@ void loop()
       break;
     }
   }
+
+if (pc1_int_flag)
+  {
+    pc1_int_flag = false;
+    Serial.print("pc1 event: ");
+    switch (pc1.event_status())
+    {
+    case evt_thres0:
+      Serial.println("Threshold 0");
+      break;
+
+    case evt_thres1:
+      Serial.println("Threshold 1");
+      break;
+
+    case evt_high_lim:
+      Serial.println("High Limit");
+      break;
+    }
+  }
   delay(50);
 }
 
-// pulse counter ISR
+// pulse counter #0 ISR
 IRAM_ATTR void pc0_isr(void *)
 {
   digitalWrite(pin_led, HIGH);
@@ -182,4 +229,16 @@ IRAM_ATTR void pc0_isr(void *)
   pc0_int_flag = true;
   // get events
   portEXIT_CRITICAL_ISR(&pcntMux0);
+}
+
+// pulse counter #1 ISR
+IRAM_ATTR void pc1_isr(void *)
+{
+  //digitalWrite(pin_led, HIGH);
+  // Prevent context switching during the interrupt service routine with an ISR spinlock
+  portENTER_CRITICAL_ISR(&pcntMux1);
+  // set interrupt flag
+  pc1_int_flag = true;
+  // get events
+  portEXIT_CRITICAL_ISR(&pcntMux1);
 }
